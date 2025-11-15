6ecfe8734dfa59188c130fffdd5ee515b93ebdb7
Shourya <shouryagupta597@gmail.com>
2025-11-15 15:05:58 +0530

    backend fix index.js

--- backend/index.js
+++ backend/index.js
@@ -11,21 +11,20 @@ app.use(express.json());
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_SERVICES_ROLE_KEY = process.env.SUPABASE_SERVICES_ROLE_KEY;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICES_ROLE_KEY;
 
-const supaAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICES_ROLE_KEY);
+const supaAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
 app.post("/generate", async (req, res) => {
   try {
     const { user_id, ingredients } = req.body;
+
     if (!user_id || !Array.isArray(ingredients) || ingredients.length === 0) {
-      return res
-        .status(400)
-        .json({ error: "user_id and ingredients[] required" });
+      return res.status(400).json({ error: "user_id and ingredients[] required" });
     }
 
-  const prompt = `
+    const prompt = `
 You are a friendly cooking assistant.
 Using ONLY these ingredients: ${ingredients.join(", ")}, plus basics like salt, oil, and water,
 create a very simple recipe.
@@ -42,27 +41,30 @@ Include:
       contents: prompt,
     });
 
-    console.log(response);
-
-    const text =  response.text;
+    const text = response.text;
 
     const [titleLine, ...rest] = text
       .trim()
       .split("\n")
       .map((l) => l.trim())
       .filter(Boolean);
+
     const recipe_text = rest.join("\n");
 
-    const { data, error: inserError } = await supaAdmin.from("recipes").insert([
-      {
-        user_id,
-        title: titleLine || "simple recipe",
-        ingredients:JSON.stringify(ingredients),
-        recipe_text,
-      },
-    ]);
+    const { data, error: insertError } = await supaAdmin
+      .from("recipes")
+      .insert([
+        {
+          user_id,
+          title: titleLine || "simple recipe",
+          ingredients: JSON.stringify(ingredients), // or just ingredients if text[]
+          recipe_text,
+        },
+      ])
+      .select();
 
-    if (inserError) {
+    if (insertError) {
+      console.log("INSERT ERROR:", insertError);
       return res.status(500).json({ error: "failed to save the recipe" });
     }
 
@@ -72,13 +74,14 @@ Include:
       saved: true,
       id: data?.[0]?.id ?? null,
     });
+
   } catch (error) {
     console.error("generate error", error);
     return res.status(500).json({ error: "server error" });
   }
 });
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("good day young one ");
+  console.log("good day young one");
 });
